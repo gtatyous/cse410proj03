80dee8ae4bc50bd63b4a922a85a483a47e5ee8a8
Yousef Gtat <gtatyous@gmail.com>
2015-04-22 17:36:58 -0400

    is not recognizing dir

--- main.cpp
+++ main.cpp
@@ -1,3 +1,30 @@
+/******************************************************
+ ** FILE: main.cpp
+ **
+ ** CLASS:
+ ** CSE410
+ **
+ ** PROJECT03 -- Extra Credits
+ **
+ ** AUTHOR:
+ ** Yousef Gtat
+ **
+ ** CREATION DATE:
+ ** 04/22/2015
+ **
+ ** Fucntions
+ ** - is_Dir: returns a bool whether it's a directory or not
+ ** - to_bizzaro: returns a reversed string of the passed argument
+ ** - is_Bizzaro: checks if the file/dir is already reversed
+ ** - BizzFile: reverse a file name and its content, then save
+ **             them in the given output file path
+ ** - BizzDir: recursively reverse a directory and all of
+ **            its sub-files and sub-directories
+ ** - main: checks if the dir/file name is given with command line.
+ **         checks if the dir/file is already reversed
+ **         if none, it creates Bizzaro version of the dir/file
+ *******************************************************/
+
 #include <iostream>
 #include <string.h>
 #include <dirent.h> // struct dirent is defined in this header
@@ -16,7 +43,8 @@ bool is_Dir (string arg, string loc)
     dir = opendir(loc.c_str());
     while((entry = readdir(dir)) != NULL)
     {
-        if (entry->d_name[0] != '.' && entry->d_name == arg && entry->d_type == DT_DIR)
+        cout << "in is_Dir; name: " << entry->d_name << endl;
+        if (entry->d_name == arg && entry->d_type == DT_DIR)
         {return true;}
     }
     closedir(dir);
@@ -41,7 +69,7 @@ bool is_Bizzaro (string arg, string loc)
     struct dirent *entry;
     dir = opendir(loc.c_str());
     
-    string bizzaro = to_bizzaro(arg);
+    string bizzaro = to_bizzaro(arg);  //reversed name of arg
     while((entry = readdir(dir)) != NULL)
     {
         if (entry->d_name == bizzaro) //Already exist
@@ -51,23 +79,28 @@ bool is_Bizzaro (string arg, string loc)
         }
     }
     closedir(dir);
-    cout <<endl;
     return false;
 }
 
-//Checks if the file is already a Bizzaro version (e.g. txt.xyz)
-bool is_BizzaroFile (string name)
+
+//Reads in a file and reverse its contents which will be written in another output file
+void BizzFile (string readFilePath, string writeFilePath)
 {
-    if (name.substr(0,4) == "txt.")
-    {
-        return true;
-    }
-    return false;
+    ifstream inFile (readFilePath);
+    stringstream strStream;
+    strStream << inFile.rdbuf(); //read the file
+    inFile.close();
+    string str = strStream.str(); //str holds the content of the file
+    string BizzaroStr = to_bizzaro(str); //reverse the content of the file
+    
+    ofstream out (writeFilePath);
+    out << BizzaroStr;
+    out.close();
+
 }
 
-
-//Recursively applies Bizzaro coding on the all subdir and files
-void readDir( string argDir, string argBizzaroDir, int level )
+//Recursively applies Bizzaro on all subdir and files
+void BizzDir( string argDir, string argBizzaroDir, int level )
 {
     //level was mostly used in debuging
     
@@ -85,62 +118,27 @@ void readDir( string argDir, string argBizzaroDir, int level )
         mkdir(argBizzaroDir.c_str(), S_IRWXU);
 
         errno = 0;
-        string name;
         while((entry = readdir(dir)) != NULL)
         {
-            /* //turns out that Bizzaro is applied for sub-directories as well
-
-            if (entry->d_name[0] != '.' && is_Bizzaro(entry->d_name, argDir))
-            {
-                //copy it to argBizzaroDir
-                continue;
-            }*/
-            
             if (entry->d_type == DT_DIR && entry->d_name[0] != '.')
             {
                 string new_argDir = argDir + "/" + entry->d_name;
                 string new_argBizzaroDir = argBizzaroDir + "/" + to_bizzaro(entry->d_name);
-                readDir (new_argDir, new_argBizzaroDir, level+1);
+                BizzDir (new_argDir, new_argBizzaroDir, level+1);
             }
             
             else if (entry->d_name[0] != '.') //it's a file
             {
-                if (is_BizzaroFile(entry->d_name))
-                {
-                    //copy it exactly the same to the new directory
-                    string readFilePath = argDir + "/" + entry->d_name;
-                    ifstream inFile (readFilePath);
-                    stringstream strStream;
-                    strStream << inFile.rdbuf(); //read the file
-                    inFile.close();
-                    string str = strStream.str(); //str holds the content of the file
-                    
-                    string outFilePath = argBizzaroDir + "/" + entry->d_name;
-                    ofstream out (outFilePath); //creates a file with the same name
-                    out << str;
-                    out.close();
-                }
-                else
-                {
-                    //otherwise apply Bizzaro on it
-                    string readFilePath = argDir + "/" + entry->d_name;
-                    ifstream inFile (readFilePath);
-                    stringstream strStream;
-                    strStream << inFile.rdbuf(); //read the file
-                    inFile.close();
-                    string str = strStream.str(); //str holds the content of the file
-                    string BizzaroStr = to_bizzaro(str); //reverse the content of the file
-                    
-                    string BizzaroName = to_bizzaro(entry->d_name);
-                    string outFilePath = argBizzaroDir + "/" + BizzaroName;
-                    ofstream out (outFilePath); //creates a file with the same name
-                    out << BizzaroStr;
-                    out.close();
-                }
-
+                //apply Bizzaro on it even if it was already reversed
+                string readFilePath = argDir + "/" + entry->d_name;
+                string BizzaroName = to_bizzaro(entry->d_name); //reversed name
+                string writeFilePath = argBizzaroDir + "/" + BizzaroName;
                 
+                //creates a file with reversed name and content of the read file
+                BizzFile(readFilePath, writeFilePath);
             }
         }
+        
         if(errno != 0)
         {
             cout << "readdir error. " << strerror(errno) <<"\t--Level: "<<level<< endl;
@@ -166,38 +164,23 @@ int main(int argc, char* argv[])
     string argBizzaro = to_bizzaro(arg);
     
     
-    
     if (is_Dir(arg, loc))
     {
+        cout << "The argument provided is a directory" <<endl;
         string argDir = "./" + arg;
         string argBizzaroDir = "./" + argBizzaro;
-        readDir (argDir, argBizzaroDir, 1);
+        BizzDir (argDir, argBizzaroDir, 1);
     }
     else //it's a file
     {
-        if (is_BizzaroFile(arg))
-        {
-            cout << "Error: can't create Bizarro file "<<arg<< "; it's already a Bizarro file!! "<<endl;
-            return 0;
-        }
-        else
-        {
-            //otherwise apply Bizzaro on it
-            string readFilePath = loc + "/" + arg;
-            ifstream inFile (readFilePath);
-            stringstream strStream;
-            strStream << inFile.rdbuf(); //read the file
-            inFile.close();
-            string str = strStream.str(); //str holds the content of the file
-            string BizzaroStr = to_bizzaro(str); //reverse the content of the file
-            
-            string outFilePath = loc + "/" + argBizzaro;
-            ofstream out (outFilePath); //creates a file with the same name
-            out << BizzaroStr;
-            out.close();
-        }
-
+        cout << "The argument provided is a directory" << endl;
+        string readFilePath = loc + "/" + arg;
+        string writeFilePath = loc + "/" + argBizzaro;
+        
+        //creates a file with reversed name and content of the read file
+        BizzFile(readFilePath, writeFilePath);
     }
+    
     cout << "Operation finished succesfully! " <<endl;
     return 0;
 }
